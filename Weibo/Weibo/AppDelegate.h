9e1de3922115170796c3ff29109aad2014fd2be2
Linfeikai <linfeikai@LinfeikaideMac.local>
2021-05-19 01:42:33 +0800

    First Commit

---
+++ Weibo/Weibo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Weibo
+//
+//  Created by Linfeikai on 2021/5/19.
+//  Copyright © 2021 Linfeikai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Weibo/Weibo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Weibo
+//
+//  Created by Linfeikai on 2021/5/19.
+//  Copyright © 2021 Linfeikai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
